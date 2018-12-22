78fd33d4facee8f224ebd61c60ffd0904fa930ee
Somee <Somee@somee-pc.mshome.net>
2018-12-22 23:05:47 +0530

    First Commit

---
+++ struts3/src/p1/Exmp2.java
@@ -0,0 +1,26 @@
+package p1;
+
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.interceptor.Interceptor;
+
+public class Exmp2 implements Interceptor {
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String intercept(ActionInvocation a1) throws Exception {
+		String temp=a1.invoke();		
+		return temp;
+	}
+
+}
---
+++ struts3/src/p1/RegisterAction.java
@@ -0,0 +1,26 @@
+package p1;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class RegisterAction extends ActionSupport {
+	private String un;
+	private String pwd;
+	public String getUn() {
+		return un;
+	}
+	public void setUn(String un) {
+		this.un = un;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public String execute() {
+		System.out.println(un+" "+pwd);
+		return "s";
+		
+	}
+
+}
