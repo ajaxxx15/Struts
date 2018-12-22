0f9a143f85ddab1fc36ca5fcc6499b460d3f8132
Somee <Somee@somee-pc.mshome.net>
2018-12-22 23:02:12 +0530

    First Commit

---
+++ struts/src/p1/RegisterAction.java
@@ -0,0 +1,39 @@
+package p1;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class RegisterAction extends ActionSupport {
+	private String un;
+	private String pwd;
+
+	public String getUn() {
+		return un;
+	}
+
+	public void setUn(String un) {
+		this.un = un;
+	}
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+	public void validate() {
+		if (un.length() == 0) {
+			addFieldError("un", "username cannot be empty");
+		}
+		if (pwd.length() == 0) {
+			addFieldError("pwd", "password cannot be empty");
+
+		}
+	}
+
+	public String execute() {
+		System.out.println(un+" "+pwd);
+		return "s";
+	}
+}
